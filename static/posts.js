7cc15427709177e0729d5c63b193bad6e101413e
Whitney Plantz <whitney.plantz96@gmail.com>
2021-01-29 08:47:33 +0000

    fix

--- static/posts.js
+++ static/posts.js
@@ -1,20 +1,18 @@
 //File:posts.js
 //Author: Carlos Sotello
 //Purpose: Fills blog page with posts
-console.log("Script loaded")
 post = { // example post until db is made
     title:"Example title",
     text:"Post text",
     author:"Whitney Plantz",
     timestamp:"1/29/21"
 }
-console.log("Example post:" + post)
 posts = [post] // represents list of posts to be made later
 let container = document.getElementById("container")
-
+/*
 while(container.firstChild)//Remove any prior blog posts 
     container.remove(container.firstChild)
-
+*/
 posts.array.forEach(post => { // create and append each post
     let title = document.createElement("h2")
     let text = document.createElement("p")
