5ec76d0db09a881eb8010e86ca4ef091a9b583e4
Whitney Plantz <whitney.plantz96@gmail.com>
2021-01-29 08:25:55 +0000

    Added posts.js

---
+++ static/posts.js
@@ -0,0 +1,35 @@
+//File:posts.js
+//Author: Carlos Sotello
+//Purpose: Fills blog page with posts
+
+post = { // example post until db is made
+    title:"Example title",
+    text:"Post text",
+    author:"Whitney Plantz",
+    timestamp:"1/29/21"
+}
+posts = [post] // represents list of posts to be made later
+let container = document.getElementById("container")
+
+while(container.firstChild)//Remove any prior blog posts 
+    container.remove(container.firstChild)
+
+posts.array.forEach(post => { // create and append each post
+    let title = document.createElement("h2")
+    let text = document.createElement("p")
+    let details = document.createElement("h5")
+    let br = document.createElement("br")
+
+    title.innerHTML = post.title
+    text.innerHTML = post.text
+    details.innerHTML = "Post by " + post.author + ", at " + post.timestamp
+
+    container.appendChild(title)
+    container.appendChild(details)
+    container.appendChild(text)
+    container.appendChild(br)
+    container.appendChild(br)
+
+});
+
+
